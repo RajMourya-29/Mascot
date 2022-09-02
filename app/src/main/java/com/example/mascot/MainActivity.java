b5c474adf6669828bc3193c38cc28a6d58727c4c
Suraj Mishra <imsuraj024@gmail.com>
2022-09-02 17:09:59 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mascot/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.mascot;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+
+import com.example.mascot.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    NavController navController;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        navController = Navigation.findNavController(this,R.id.main);
+        NavigationUI.setupActionBarWithNavController(this,navController);
+
+    }
+}
---
+++ app/src/main/java/com/example/mascot/retrofit/ApiInterface.java
@@ -0,0 +1,6 @@
+package com.example.mascot.retrofit;
+
+public interface ApiInterface
+{
+    String BASE_URL = "http://asahiws.rajkamalbarscan.co.in/WebService1.asmx/";
+}
